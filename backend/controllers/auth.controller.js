e0621df5b4ac89ed20e0672bcf4cbde89a343908
jialiangwu4 <jialiang.wu4@gmail.com>
2024-12-08 09:33:35 -0600

    add basic auth routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+  res.json({ data: "this is signup page. Work in progress" });
+};
+export const login = async (req, res) => {
+  res.json({ data: "this is login page. Work in progress" });
+};
+export const logout = async (req, res) => {
+  res.json({ data: "this is logout page. Work in progress" });
+};
