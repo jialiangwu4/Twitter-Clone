013fde92ed08409107d2c725482c246b3c4f545d
jialiangwu4 <jialiang.wu4@gmail.com>
2024-12-10 07:57:47 -0600

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // MongoDB has ObjectId
+        ref: "User",
+        default: [],
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    profileImg: {
+      type: String,
+      default: "",
+    },
+    coverImg: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true } // this will add createdAt and updatedAt fields to the schema
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
