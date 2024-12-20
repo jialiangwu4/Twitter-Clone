3fdf29050c48fbae068435975facd5d26effc67b
jialiangwu4 <jialiang.wu4@gmail.com>
2024-12-20 12:37:54 -0600

    add /api/users/follow/:id

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,3 +13,63 @@ export const getUserProfile = async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const followUnfollowUser = async (req, res) => {
+  const { id } = req.params; // user to be followed or unfollowed
+  const { _id } = req.user; // me
+
+  try {
+    const user = await User.findById(id); // Get the user to be followed or unfollowed
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const me = await User.findById(_id); // Get the user who is making the request
+    if (!me) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Check if the user is trying to follow/unfollow themselves
+    if (id === _id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You cannot follow/unfollow yourself" });
+    }
+
+    // Check if me is already following the other user
+    const isFollowing = me.following.includes(id);
+    if (isFollowing) {
+      // Unfollow the user
+      await User.findByIdAndUpdate(
+        _id,
+        { $pull: { following: id } },
+        { new: true }
+      );
+      await User.findByIdAndUpdate(
+        id,
+        { $pull: { followers: _id } },
+        { new: true }
+      );
+      return res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      // Follow the user
+      await User.findByIdAndUpdate(
+        _id,
+        { $push: { following: id } },
+        { new: true }
+      );
+      await User.findByIdAndUpdate(
+        id,
+        { $push: { followers: _id } },
+        { new: true }
+      );
+
+      // TODO: send notification
+      //
+
+      return res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { protectedRoute } from "../middleware/protectedRoute.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  getUserProfile,
+  followUnfollowUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/profile/:username", protectedRoute, getUserProfile);
+router.get("/profile/:username", protectedRoute, getUserProfile); // Get user profile
+router.post("/follow/:id", protectedRoute, followUnfollowUser); // Follow or unfollow a user
 // router.get("/suggested", protectedRoute, getSuggestedUsers);
-// router.get("/following/:id", protectedRoute, getFollowingUsers);
 // router.get("/update", protectedRoute, updateUserProfile);
 export default router;
