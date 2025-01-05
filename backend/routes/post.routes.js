08cd3a90c6e32dd4e9d9e570a1134f1890825692
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-04 19:43:03 -0600

    add comment to post.routes.js

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -11,11 +11,11 @@ import {
 
 const router = express.Router();
 
-router.get("/all", protectedRoute, getAllPosts);
-router.get("/likes/:id", protectedRoute, getLikedPosts);
+router.get("/all", protectedRoute, getAllPosts); // this might need update
+router.get("/likes/:id", protectedRoute, getLikedPosts); //user id
 router.post("/create", protectedRoute, createPost);
-router.post("/like/:id", protectedRoute, likeUnlikePost);
-router.post("/comment/:id", protectedRoute, commentPost);
-router.delete("/:id", protectedRoute, deletePost);
+router.post("/like/:id", protectedRoute, likeUnlikePost); //post id
+router.post("/comment/:id", protectedRoute, commentPost); //post id
+router.delete("/:id", protectedRoute, deletePost); //post id
 
 export default router;
