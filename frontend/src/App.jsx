717399662ebf7dec53e85bc1a3f344d648cd29d3
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-18 15:25:56 -0600

    create dummy data, add right panel and its skeletons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
 import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
