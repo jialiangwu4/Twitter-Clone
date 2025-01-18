cd420eee04de9177e86fc7ab074e195744db007e
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-18 14:47:16 -0600

    add Homepage, sidebar, and boilerplate for Posts and CreatePost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
+import Sidebar from "./components/common/Sidebar.jsx";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* every compenent that is outside of routes is a common component that will be used in all pages */}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
