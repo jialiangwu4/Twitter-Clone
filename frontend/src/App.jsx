63089f8ec3aa9eb386027ff20554a3c4bef7e36a
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-19 16:51:34 -0600

    add Notification Page and LoadinngSpinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/auth/login/LoginPage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
 import Sidebar from "./components/common/Sidebar.jsx";
 import RightPanel from "./components/common/RightPanel.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
       <RightPanel />
     </div>
