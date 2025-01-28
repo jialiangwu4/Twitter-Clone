011808a339585071ca86d59dd2cfcce766d78c6d
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-27 21:43:03 -0600

    update formattedDate

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
@@ -144,7 +145,7 @@ const Post = ({ post }) => {
   // check if the current user is the owner of the post
   const isMyPost = authUser?._id === postOwner._id;
 
-  const formattedDate = "1h";
+  const formattedDate = formatPostDate(post.createdAt);
 
   const handleDeletePost = () => {
     deletePost();
