88d7fc9e69103424850937c2f574bc5a1cba1b8a
jialiangwu4 <jialiang.wu4@gmail.com>
2025-01-27 22:12:21 -0600

    update NotificationPage to actually get and delete all notifications

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,35 +1,67 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
 const NotificationPage = () => {
-  const isLoading = false;
-  const notifications = [
-    {
-      _id: "1",
-      from: {
-        _id: "1",
-        username: "johndoe",
-        profileImg: "/avatars/boy2.png",
-      },
-      type: "follow",
+  const queryClient = useQueryClient();
+
+  // get notifications
+  const { data: notifications, isLoading: isNotificationsLoading } = useQuery({
+    queryKey: ["notifications"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/notifications");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     },
-    {
-      _id: "2",
-      from: {
-        _id: "2",
-        username: "janedoe",
-        profileImg: "/avatars/girl1.png",
-      },
-      type: "like",
+  });
+
+  // delete all notifications
+  const {
+    mutate: deleteNotificationsMutate,
+    isPending: isDeleting,
+    error,
+  } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/notifications", {
+          method: "DELETE",
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     },
-  ];
+    onSuccess: () => {
+      toast.success("All notifications deleted");
+      // invalidate the query to update the UI with the new data
+      queryClient.invalidateQueries({ queryKey: ["notifications"] });
+    },
+
+    onError: () => {
+      toast.error(error.message);
+    },
+  });
 
   const deleteNotifications = () => {
-    alert("All notifications deleted");
+    if (isDeleting) return;
+    deleteNotificationsMutate();
   };
 
   return (
@@ -51,7 +83,7 @@ const NotificationPage = () => {
             </ul>
           </div>
         </div>
-        {isLoading && (
+        {isNotificationsLoading && (
           <div className="flex justify-center h-full items-center">
             <LoadingSpinner size="lg" />
           </div>
